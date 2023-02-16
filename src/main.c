f93d10c58efd50de284f347da870789971951f9c
osama_shafique <bc190200177@vu.edu.pk>
2023-02-16 10:08:33 +0500

    full duplex feature added in ethernet hardware

--- src/main.c
+++ src/main.c
@@ -1,19 +1,34 @@
 #include <stdio.h>
 #include <string.h>
-#include "sdkconfig.h"
+#include <esp_eth_enc28j60.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/uart.h"
+#include "esp_netif.h"
 #include "esp_eth.h"
 #include "esp_event.h"
 #include "esp_log.h"
 #include "driver/gpio.h"
-#include "esp_eth_enc28j60.h"
 #include "driver/spi_master.h"
 
 
+#define ENC_MOSI_PIN GPIO_NUM_13
+#define ENC_MISO_PIN GPIO_NUM_12
+#define ENC_CLK_PIN  GPIO_NUM_14
+#define ENC_CS_PIN   GPIO_NUM_15
+#define ENC_INT_PIN  GPIO_NUM_4
+#define ENC_SPI_CLOCK_MHZ (8)
+
+#define ENC_SPI_HOST SPI2_HOST
+
+#define RED_LED_PIN GPIO_NUM_5
+#define GREEN_LED_PIN GPIO_NUM_17
+#define BLUE_LED_PIN GPIO_NUM_16
+#define BUZZER_PIN GPIO_NUM_22
 
 static const char *TAG = "SWH_eth_test";
+
+void rgbConfig(void);  // rgb led configuration
+void setRGBLED(uint8_t , uint8_t , uint8_t);  // setting individual leds
 
 /** Event handler for Ethernet events */
 static void eth_event_handler(void *arg, esp_event_base_t event_base,
@@ -32,9 +47,11 @@ static void eth_event_handler(void *arg, esp_event_base_t event_base,
         break;
     case ETHERNET_EVENT_DISCONNECTED:
         ESP_LOGI(TAG, "Ethernet Link Down");
+        setRGBLED(0 , 1 , 1);
         break;
     case ETHERNET_EVENT_START:
         ESP_LOGI(TAG, "Ethernet Started");
+        setRGBLED(1 , 1 , 0);
         break;
     case ETHERNET_EVENT_STOP:
         ESP_LOGI(TAG, "Ethernet Stopped");
@@ -57,10 +74,13 @@ static void got_ip_event_handler(void *arg, esp_event_base_t event_base,
     ESP_LOGI(TAG, "ETHMASK:" IPSTR, IP2STR(&ip_info->netmask));
     ESP_LOGI(TAG, "ETHGW:" IPSTR, IP2STR(&ip_info->gw));
     ESP_LOGI(TAG, "~~~~~~~~~~~");
+    setRGBLED(1 , 0 , 1);
 }
 
 void app_main(void)
 {
+    rgbConfig(); // configuring rgb leds
+
     ESP_ERROR_CHECK(gpio_install_isr_service(0));
     // Initialize TCP/IP network interface (should be called only once in application)
     ESP_ERROR_CHECK(esp_netif_init());
@@ -70,45 +90,46 @@ void app_main(void)
     esp_netif_t *eth_netif = esp_netif_new(&netif_cfg);
 
     spi_bus_config_t buscfg = {
-        .miso_io_num = CONFIG_EXAMPLE_ENC28J60_MISO_GPIO,
-        .mosi_io_num = CONFIG_EXAMPLE_ENC28J60_MOSI_GPIO,
-        .sclk_io_num = CONFIG_EXAMPLE_ENC28J60_SCLK_GPIO,
+        .miso_io_num = ENC_MISO_PIN,
+        .mosi_io_num = ENC_MOSI_PIN,
+        .sclk_io_num = ENC_CLK_PIN,
         .quadwp_io_num = -1,
         .quadhd_io_num = -1,
     };
-    ESP_ERROR_CHECK(spi_bus_initialize(CONFIG_EXAMPLE_ENC28J60_SPI_HOST, &buscfg, SPI_DMA_CH_AUTO));
+    ESP_ERROR_CHECK(spi_bus_initialize(ENC_SPI_HOST, &buscfg, SPI_DMA_CH_AUTO));
     /* ENC28J60 ethernet driver is based on spi driver */
     spi_device_interface_config_t spi_devcfg = {
         .mode = 0,
-        .clock_speed_hz = CONFIG_EXAMPLE_ENC28J60_SPI_CLOCK_MHZ * 1000 * 1000,
-        .spics_io_num = CONFIG_EXAMPLE_ENC28J60_CS_GPIO,
+        .clock_speed_hz = ENC_SPI_CLOCK_MHZ * 1000 * 1000,
+        .spics_io_num = ENC_CS_PIN,
         .queue_size = 20,
-        .cs_ena_posttrans = enc28j60_cal_spi_cs_hold_time(CONFIG_EXAMPLE_ENC28J60_SPI_CLOCK_MHZ),
+        .cs_ena_posttrans = enc28j60_cal_spi_cs_hold_time(ENC_SPI_CLOCK_MHZ),
     };
 
-    eth_enc28j60_config_t enc28j60_config = ETH_ENC28J60_DEFAULT_CONFIG(CONFIG_EXAMPLE_ENC28J60_SPI_HOST, &spi_devcfg);
-    enc28j60_config.int_gpio_num = CONFIG_EXAMPLE_ENC28J60_INT_GPIO;
+    eth_enc28j60_config_t enc28j60_config = ETH_ENC28J60_DEFAULT_CONFIG(ENC_SPI_HOST, &spi_devcfg);
+    enc28j60_config.int_gpio_num = ENC_INT_PIN;
 
     eth_mac_config_t mac_config = ETH_MAC_DEFAULT_CONFIG();
     esp_eth_mac_t *mac = esp_eth_mac_new_enc28j60(&enc28j60_config, &mac_config);
 
     eth_phy_config_t phy_config = ETH_PHY_DEFAULT_CONFIG();
-    phy_config.autonego_timeout_ms = 0; 
-    phy_config.reset_gpio_num = -1; 
+    phy_config.autonego_timeout_ms = 0; // ENC28J60 doesn't support auto-negotiation
+    phy_config.reset_gpio_num = -1; // ENC28J60 doesn't have a pin to reset internal PHY
     esp_eth_phy_t *phy = esp_eth_phy_new_enc28j60(&phy_config);
 
     esp_eth_config_t eth_config = ETH_DEFAULT_CONFIG(mac, phy);
     esp_eth_handle_t eth_handle = NULL;
     ESP_ERROR_CHECK(esp_eth_driver_install(&eth_config, &eth_handle));
 
-    // ENC28J60 doesn't burn any factory MAC address, we need to set it manually.
-     
+    /* ENC28J60 doesn't burn any factory MAC address, we need to set it manually.
+       02:00:00 is a Locally Administered OUI range so should not be used except when testing on a LAN under your control.
+    */
     mac->set_addr(mac, (uint8_t[]) {
         0x02, 0x00, 0x00, 0x12, 0x34, 0x56
     });
 
-    // ENC28J60 datasheep #1 check
-    if (emac_enc28j60_get_chip_info(mac) < ENC28J60_REV_B5 && CONFIG_EXAMPLE_ENC28J60_SPI_CLOCK_MHZ < 8) {
+    // ENC28J60 Errata #1 check
+    if (emac_enc28j60_get_chip_info(mac) < ENC28J60_REV_B5 && ENC_SPI_CLOCK_MHZ < 8) {
         ESP_LOGE(TAG, "SPI frequency must be at least 8 MHz for chip revision less than 5");
         ESP_ERROR_CHECK(ESP_FAIL);
     }
@@ -118,7 +139,8 @@ void app_main(void)
     // Register user defined event handers
     ESP_ERROR_CHECK(esp_event_handler_register(ETH_EVENT, ESP_EVENT_ANY_ID, &eth_event_handler, NULL));
     ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_ETH_GOT_IP, &got_ip_event_handler, NULL));
-//  full duplex or half duflex mode configuration //
+
+    /* It is recommended to use ENC28J60 in Full Duplex mode since multiple errata exist to the Half Duplex mode */
 #if CONFIG_EXAMPLE_ENC28J60_DUPLEX_FULL
     eth_duplex_t duplex = ETH_DUPLEX_FULL;
     ESP_ERROR_CHECK(esp_eth_ioctl(eth_handle, ETH_CMD_S_DUPLEX_MODE, &duplex));
@@ -126,4 +148,34 @@ void app_main(void)
 
     /* start Ethernet driver state machine */
     ESP_ERROR_CHECK(esp_eth_start(eth_handle));
+    //vTaskDelay(pdMS_TO_TICKS(100));
+}
+void rgbConfig(void)
+{
+    // resetting all the rgb leds 
+    gpio_reset_pin(RED_LED_PIN);
+    gpio_reset_pin(GREEN_LED_PIN);
+    gpio_reset_pin(BLUE_LED_PIN);
+    gpio_reset_pin(BUZZER_PIN);
+    // setting gpio mode of the rgb pins
+    gpio_set_direction(RED_LED_PIN , GPIO_MODE_OUTPUT);
+    gpio_set_direction(GREEN_LED_PIN , GPIO_MODE_OUTPUT);
+    gpio_set_direction(BLUE_LED_PIN , GPIO_MODE_OUTPUT);
+    gpio_set_direction(BUZZER_PIN , GPIO_MODE_OUTPUT);
+
+    gpio_set_level(RED_LED_PIN , 0);   // set red led by default
+
+
+}
+
+void setRGBLED(uint8_t red , uint8_t green , uint8_t blue)
+{ 
+    gpio_set_level(RED_LED_PIN , red);
+    gpio_set_level(RED_LED_PIN , green);
+    gpio_set_level(RED_LED_PIN , blue);
+
+
+
+
+
 }
